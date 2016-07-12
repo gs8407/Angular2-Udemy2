6ff31177e7efe96def179dbda9fa8e9c6497f3eb
Peter Bankuti <peter.bankuti@techniatranscat.com>
2016-07-12 16:26:22 +0200

    Created an ng2 component to hold the three JS test code

--- Angular2-Project/src/app/app.component.ts
+++ Angular2-Project/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { ThreeJsComponent } from './threeJS/threeJS.component';
+
 
 @Component({
   moduleId: module.id,
   selector: 'pb-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives:[ThreeJsComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'ThreeJS testing';
 }
---
+++ Angular2-Project/src/app/threeJS/threeJS.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  moduleId: module.id,
+  selector:'pb-threeJS',
+  templateUrl: 'threeJS.component.html',
+  styleUrls: ['threeJS.component.css']
+})
+
+
+export class ThreeJsComponent {
+
+}
