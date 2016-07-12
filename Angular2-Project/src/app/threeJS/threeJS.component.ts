d152b8ab9ca70957a61d6bdcde06ffdd89ac9d1b
Peter Bankuti <peter.bankuti@techniatranscat.com>
2016-07-12 17:31:13 +0200

    Add and remove 3JS dom  element from div.

--- Angular2-Project/src/app/threeJS/threeJS.component.ts
+++ Angular2-Project/src/app/threeJS/threeJS.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild, ElementRef} from "@angular/core";
 
 
 @Component({
@@ -10,5 +10,26 @@ import {Component} from "@angular/core";
 
 
 export class ThreeJsComponent {
+    @ViewChild('myContent') my_content: ElementRef;
 
+    
+    renderer: THREE.WebGLRenderer;
+    constructor(){
+    
+        this.renderer = new THREE.WebGLRenderer({ alpha: true });
+        this.renderer.setSize(100,100);
+        this.renderer.setClearColor(0xFF0000,1);
+        this.renderer.clear();  // TO ASK: what is this? 
+    }
+
+    onAppend3JS(){
+        console.log("Appended 3JS to div.");        
+        this.my_content.nativeElement.appendChild(this.renderer.domElement);
+    }
+
+    onRemove3JS(){
+        console.log("Removed 3JS from div.");
+        this.my_content.nativeElement.removeChild(this.renderer.domElement);
+    }
 }
+
