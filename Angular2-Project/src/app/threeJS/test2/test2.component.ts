66b9a9707ae8b91e1c34ba7fc2dca396fec239d2
Padamban <peter.adam.b@gmail.com>
2016-07-13 21:54:29 +0200

    Added helper axis and moved the camera.

--- Angular2-Project/src/app/threeJS/test2/test2.component.ts
+++ Angular2-Project/src/app/threeJS/test2/test2.component.ts
@@ -18,9 +18,7 @@ export class ThreeJs_Test2_Component {
    
     @ViewChild('myTestContent') my_content: ElementRef; 
 
-
-
-    
+ 
     renderer: THREE.WebGLRenderer;
     scene: THREE.Scene;
     camera: THREE.Camera;
@@ -28,10 +26,14 @@ export class ThreeJs_Test2_Component {
     material: THREE.MeshBasicMaterial;
     cube: THREE.Mesh;
 
+    axis: THREE.AxisHelper;
+
+
+
     constructor(){
     
         this.renderer = new THREE.WebGLRenderer({ alpha: true });   
-        this.renderer.setSize( window.innerWidth/3, window.innerHeight/3 );                             
+        this.renderer.setSize( window.innerWidth/2, window.innerHeight/2 );                             
         this.renderer.setClearColor(0xF070F7,1);                   
         this.renderer.clear();
        
@@ -39,13 +41,24 @@ export class ThreeJs_Test2_Component {
         this.scene = new THREE.Scene();                             
 
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth /window.innerHeight, 0.1, 1000);
-        this.camera.position.z = 5;
+        this.camera.position.x = 20;
+        this.camera.position.y = 20;
+        this.camera.position.z = 20;
+        this.camera.lookAt(this.scene.position);
 
-        this.boxGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+
+        this.boxGeometry = new THREE.BoxGeometry( 5, 5, 5 );
         this.material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
         this.cube = new THREE.Mesh( this.boxGeometry, this.material );
-
+        this.cube.position.x = 10;
+        this.cube.position.y = 0;
+        this.cube.position.z = 0;
         this.scene.add(this.cube);
+        
+        this.axis = new THREE.AxisHelper(20);
+        this.scene.add(this.axis);
+
+
 
     }
 
