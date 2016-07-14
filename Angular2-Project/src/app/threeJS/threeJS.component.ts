14e3f43c1e93a68a51ed358ee69c788401a45e82
Peter Bankuti <peter.bankuti@techniatranscat.com>
2016-07-14 08:49:03 +0200

    Rotate mesh.

--- Angular2-Project/src/app/threeJS/threeJS.component.ts
+++ Angular2-Project/src/app/threeJS/threeJS.component.ts
@@ -2,6 +2,7 @@ import {Component, ViewChild, ElementRef} from "@angular/core";
 import { ThreeJs_AppendRemoveChild_Component } from './test/append-remove-child.component';
 import { ThreeJs_DisplayMesh_Component } from './test/display-mesh.component';
 import { ThreeJs_AddTexture_Component } from './test/add-texture.component';
+import { ThreeJs_RotateMesh_Component } from './test/rotate-mesh.component';
 import { ThreeJs_Test_Component } from './test/test.component';
 
 
@@ -14,6 +15,7 @@ import { ThreeJs_Test_Component } from './test/test.component';
         ThreeJs_AppendRemoveChild_Component,
         ThreeJs_DisplayMesh_Component, 
         ThreeJs_AddTexture_Component,
+        ThreeJs_RotateMesh_Component,
         ThreeJs_Test_Component
       ]
 })
